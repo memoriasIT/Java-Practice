639cd2749e146e544bc56c18ea183e9c13ae9f0e
Memorias de un informático <32130278+memoriasIT@users.noreply.github.com>
2018-12-30 17:55:02 +0100

    SparseVector

---
+++ SparseVector/VectorException.java
@@ -0,0 +1,10 @@
+package dataStructures.vector;
+
+@SuppressWarnings("serial")
+public class VectorException extends RuntimeException {
+
+    public VectorException(String message) {
+        super(message);
+    }
+}
+
